bb9486f7a282142241bd326bca04599b88a0a45b
Arvid J. Kingl <akingl2016@gmail.com>
2018-03-29 22:09:51 +0100

    adds zone choice

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-gcloud compute instances create consultant-workstation --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud  --machine-type n1-standard-4 --zone europe-west1-d  --metadata-from-file startup-script=Rserver_startup.sh
-gcloud compute firewall-rules create allow-dfc --allow=tcp:8787
+
+if [ -z "$1" ]; then
+    echo 'Please specify the zone as in "./start_server.sh europe-west1-d"'
+else
+    gcloud compute instances create consultant-workstation --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud  --machine-type n1-standard-4 --zone $1  --metadata-from-file startup-script=Rserver_startup.sh
+    gcloud compute firewall-rules create allow-dfc --allow=tcp:8787
+fi
