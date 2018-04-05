e31539093fb0897fbaf52dbdc41c31a37ba7c181
Arvid J. Kingl <akingl2016@gmail.com>
2018-04-05 23:35:18 +0100

    changes default number of cores on the machine

--- start_server.sh
+++ start_server.sh
@@ -3,6 +3,6 @@
 if [ -z "$1" ]; then
     echo 'Please specify the zone as in "./start_server.sh europe-west1-d"'
 else
-    gcloud compute instances create consultant-workstation --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud  --machine-type n1-standard-4 --zone $1  --metadata-from-file startup-script=Rserver_startup.sh
+    gcloud compute instances create consultant-workstation --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud  --machine-type n1-standard-2 --zone $1  --metadata-from-file startup-script=Rserver_startup.sh
     gcloud compute firewall-rules create allow-dfc --allow=tcp:8787
 fi
