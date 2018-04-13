feb268768950b8dae958d7ec8d8a2c61de788185
Arvid J. Kingl <akingl2016@gmail.com>
2018-04-13 05:10:05 +0100

    adds initialisation of GCS_AUTH_FILE variable in Renviron on startup

--- Rserver_startup.sh
+++ Rserver_startup.sh
@@ -9,3 +9,4 @@ apt install -y libgeos-dev libproj-dev libgdal-dev libudunits2-dev
 apt install -y gdebi-core
 wget https://download2.rstudio.org/rstudio-server-1.0.143-amd64.deb
 yes  | gdebi rstudio-server-1.0.143-amd64.deb
+echo "GCS_AUTH_FILE=/usr/local/key.json" >> /usr/lib/R/etc/Renviron
